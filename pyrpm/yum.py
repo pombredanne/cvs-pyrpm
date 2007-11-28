f34cc0f6879eff7811c6820e1843e55602dda7aa
Phil Knirsch <pknirsch@redhat.com>
2007-11-28 15:29:37 +0000

    - Implement manual symlink resolving and caching for buildroot installs.

--- pyrpm/yum.py
+++ pyrpm/yum.py
@@ -554,7 +554,7 @@ class RpmYum:
                                          do_obsolete=do_obsolete)
                 # In case we had a successfull update make sure we erase the
                 # package we just updated. Otherwise cross arch switches won't
-                # work properly.
+                # work properly with our current resolver.
                 if r > 0 and ipkg in self.opresolver.getDatabase():
                     self.opresolver.erase(ipkg)
                 ret |= r
